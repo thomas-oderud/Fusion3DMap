b1b3c15940530a8d55d82bff22f1236bfdf44eb0
Thomas Øderud <thomas@oederud.com>
2026-03-15 11:24:30 +0100

    Small fixes

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ from utils import *
 from dotenv import load_dotenv
 
 
-gpxdirectory, imagedirectory, downloaddirectory = checkDirectories()
-
-mapbuilder = MapBuilder(mapname='krsultra', zoom=15, minelevation=0, maxelevation=750)
-#mapbuilder.forcereprocesselevation = True
-gpxfile = os.path.join(gpxdirectory, 'KRSUltra60.gpx')
-mapbuilder.filesources.addSource(GpxSource(gpxfile, animate=True, addwaypointsasmarkers=True))
-
-mapbuilder.tilesources.setElevationSource(1) #
-mapbuilder.tilesources.setImageSource(2) # 
-
 # Add .env file and add an entry for you api-key if the source requires it
 MAPTILER_API_KEY = os.getenv("MAPTILER_API_KEY")
+gpxdirectory, imagedirectory, downloaddirectory = checkDirectories()
+
+
+###### Change to your prefences #########
+
+mapbuilder = MapBuilder(mapname='krsultra', zoom=15, minelevation=0, maxelevation=750)
+gpxfile = os.path.join(gpxdirectory, 'KRSUltra60.gpx')
+mapbuilder.filesources.addSource(GpxSource(gpxfile, animate=True, addwaypointsasmarkers=True))
+mapbuilder.tilesources.setElevationSource(1) # See slippytiles for options
+mapbuilder.tilesources.setImageSource(2) # See slippytiles for options
+mapbuilder.addtilesarounddetailed = 2 # How many extra non-detailed tiles to add around the detailed area?
+
+#########################################
+
 mapbuilder.tilesources.ImageSources[2].apikey = MAPTILER_API_KEY
 mapbuilder.tilesources.ElevationSources[1].apikey = MAPTILER_API_KEY
-
-mapbuilder.addtilesarounddetailed = 2
 
 mapbuilder.calculateTiles()
 mapbuilder.fetchTiles()
