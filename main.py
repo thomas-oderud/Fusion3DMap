8b03226b64127fe7aaba2eb80bf50c3a7e4c5912
Thomas Øderud <thomas@oederud.com>
2026-03-15 20:31:33 +0100

    Fixed elevation tile issues

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ gpxdirectory, imagedirectory, downloaddirectory = checkDirectories()
 
 ###### Change to your prefences #########
 
-mapbuilder = MapBuilder(mapname='krsultra', zoom=15, minelevation=0, maxelevation=750)
+mapbuilder = MapBuilder(mapname='krsultra', zoom=15, minelevation=0, maxelevation=750, margin_around_geometry_sources=1000)
 gpxfile = os.path.join(gpxdirectory, 'KRSUltra60.gpx')
 mapbuilder.filesources.addSource(GpxSource(gpxfile, animate=True, addwaypointsasmarkers=True))
 mapbuilder.tilesources.setElevationSource(1) # See slippytiles for options
 mapbuilder.tilesources.setImageSource(2) # See slippytiles for options
-mapbuilder.addtilesarounddetailed = 1 # How many extra non-detailed tiles to add around the detailed area?
+mapbuilder.addtilesarounddetailed = 2 # How many extra non-detailed tiles to add around the detailed area?
 
 #########################################
 
